9ea8a124f57754aa9f48507f419868ef718010b4
SylwekD <rutki2268@gmail.com>
2023-05-21 17:06:46 +0200

    add class zawodnik

---
+++ Kopakabana/Zawodnik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Kopakabana
+{
+	class Zawodnik : Osoba
+	{
+		private int NumerKoszulki;
+
+		public Zawodnik(string name, string surname, int numerkoszulki) : base(name, surname)
+		{
+			NumerKoszulki = numerkoszulki;
+		}
+
+		public int GetNumerKoszulki() 
+		{ 
+			return NumerKoszulki;
+		}
+	}
+}

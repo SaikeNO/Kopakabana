433788f3fd88e08b9bd1c8f1a983cdcdd1a3c8a9
SylwekD <rutki2268@gmail.com>
2023-05-21 17:19:28 +0200

    add class druzyna

---
+++ Kopakabana/Druzyna.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Kopakabana
+{
+	class Druzyna
+	{
+		private List<Zawodnik> Zawodnicy = new ();
+		private string Nazwa;
+
+		public Druzyna(string Nazwa)
+		{
+			this.Nazwa = Nazwa;
+		}
+
+		public Druzyna(string Nazwa, List<Zawodnik> lista)
+		{
+			this.Nazwa = Nazwa;
+			this.Zawodnicy.AddRange(lista);
+		}
+
+		public List<Zawodnik> GetZawodnicy()
+		{
+			return Zawodnicy;
+		}
+
+		public void DodajZawodnika(Zawodnik zawodnik)
+		{
+			List.Add(zawodnik);
+		}
+
+        public string Nazwa{ get { return Nazwa; } set { Nazwa = value; } }
+
+    }
+}

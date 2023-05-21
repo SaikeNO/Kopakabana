94e2d243741e1d2c4a17bdeece2fd49045ab9209
Sylwester312 <sylwester.kowalewski12@gmail.com>
2023-05-21 17:22:17 +0200

    zmiana osoby

--- Kopakabana/Osoba.cs
+++ Kopakabana/Osoba.cs
@@ -8,13 +8,15 @@ namespace Kopakabana
 {
     class Osoba
     {
-        protected string name;
-        protected string surname;
+        private string name;
+        private string surname;
 
         public Osoba(string name, string surname)
         {
             this.name = name;
             this.surname = surname;
         }
+        public string GetName() { return name; }
+        public string GetSurname() {  return surname; }
     }
 }
